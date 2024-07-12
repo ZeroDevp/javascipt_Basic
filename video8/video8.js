7c9711c94c5efbe866ebc46ce640c1cb1d3423bd
Nguyen Trong Hieu <hieunguyen6231@gmail.com>
2024-07-12 13:47:33 +0700

    video8 learn null undefined

---
+++ video8/video8.js
@@ -0,0 +1,9 @@
+console.log('Hello world from HTML')
+
+let a = 5;
+let b = '5';
+
+console.log('compare a == b: ', 'a = ', a, 'b = ', b, 'result: ', a == b)
+console.log('compare a === b: ', 'a = ', a, 'b = ', b, 'result: ', a === b)
+
+//empty, null, undefined
