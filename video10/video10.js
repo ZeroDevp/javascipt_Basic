fc6b8cbdc97d140925299c148ec0e4951bb16918
Nguyen Trong Hieu <hieunguyen6231@gmail.com>
2024-07-16 20:50:38 +0700

    while loop

---
+++ video10/video10.js
@@ -0,0 +1,10 @@
+let arrtop4 = ['man city', 'liver pool', 'mu', 'chelsea'];
+// for (let i = 0; i < arrtop4.length; i++) {
+//     console.log('top: ', i + 1, arrtop4[i])
+// }
+
+let i = 0;
+do {
+    console.log('top: ', i + 1, arrtop4[i])
+    i++
+} while (i < 0)
